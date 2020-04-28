31a2b88f1eaecc704c3a949db7a2d2f1abaad2b8
Kartikeya Srivastava <kartikeyasrivastava861@gmail.com>
2020-04-28 06:08:10 +0530

    Add files via upload

---
+++ 791A.cpp
@@ -0,0 +1,41 @@
+	/**
+ * 791 A codeforces
+ * Kartikeya (doomsday861)
+**/
+#include<bits/stdc++.h>
+#include<time.h>
+#define ll long long
+#define testcase ll t;cin>>t;while(t--)
+#define timeb  auto start = high_resolution_clock::now(); 
+#define timee auto stop = high_resolution_clock::now();auto duration = duration_cast<seconds>(stop - start);cout << "Time taken by function: "<<duration.count() << "seconds" << endl; 
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE 
+freopen("in.txt", "r", stdin); 
+freopen("output.ans", "w", stdout); 
+#endif 
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+using namespace std::chrono;
+//timeb
+	ll a,b;
+
+    while (cin >> a >> b)
+    {
+        int count = 0;
+
+        while (a <= b)
+        {
+            a = 3*a;
+            b = 2*b;
+
+            count++;
+        }
+
+        cout << count << endl;
+    }
+//timee
+    return 0; 
+} 
